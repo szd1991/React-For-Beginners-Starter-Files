110abc7ac11bd597da583b769a71add63fad32f3
szmsdnl <szamosidani@gmail.com>
2018-05-22 17:26:50 +0200

    add missing semicolon, completed lesson 5

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker.js';
+import "./css/style.css";
 
 render(<StorePicker />, document.getElementById('main'));
