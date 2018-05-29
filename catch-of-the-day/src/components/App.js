501c9db8c9df4e421c34c6d1490c04bc877f20b4
szmsdnl <szamosidani@gmail.com>
2018-05-29 15:51:29 +0200

    complete lesson 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     state = {
@@ -20,6 +21,10 @@ class App extends React.Component {
         });
     };
 
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    }
+
     render () {
         return (
             <div className="catch-of-the-day">
@@ -28,7 +33,7 @@ class App extends React.Component {
                     
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
     }
